55ba0e47ad628d6c099df81a6fcbd4c60438fa42
Satish <spd61185@gmail.com>
2021-05-03 06:28:43 +0530

    Updated configure secuirty

--- InMemorySpringSecurityWithRoles/src/main/java/com/spd/inmemory/ConfigureSecurity.java
+++ InMemorySpringSecurityWithRoles/src/main/java/com/spd/inmemory/ConfigureSecurity.java
@@ -2,6 +2,7 @@ package com.spd.inmemory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -24,6 +25,16 @@ public class ConfigureSecurity extends WebSecurityConfigurerAdapter	{
 			.roles("ADMIN");
 	}
 	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/user").hasRole("USER")
+			.antMatchers("/admin").hasAnyRole("USER","ADMIN")
+			.antMatchers("/").permitAll()
+			.and()
+			.formLogin();
+	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
